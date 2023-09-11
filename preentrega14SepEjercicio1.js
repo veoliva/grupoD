66b1f615359c76cbb5406ce1c38f1818fa30916a
veoliva <veolivatorre@gmail.com>
2023-09-11 18:54:56 -0300

    preentrega realizada en sesion de discord por alumnas, fernanda, guadalupe y veronica

---
+++ preentrega14SepEjercicio1.js
@@ -0,0 +1,23 @@
+//promp y alert (en pagina index) 
+
+
+let nombre = prompt ("ingrese su nombre ");
+let apellido = prompt ("ingrese su apellido ");
+alert ("hola" + " " + nombre + " " + apellido + " " + "bienvenido/a a la página del grupo D del curso ingenias");
+
+
+
+//  funcion donacion  ( en pagina index) (para ver el siguiente ejercicio comente el anterior /*)
+
+/*
+let donar = Number ( prompt ("deseas realizar una donación? \n1- SI \n2- NO"));
+  
+if (donar === 1 ) {
+alert ("para mas informacion  sobre como donar complete el formulario en la pagina de contacto");
+} 
+else {
+alert ("Muchas gracias por su visita");
+console.log("Muchas gracias por su visita");
+}
+*/
+     
