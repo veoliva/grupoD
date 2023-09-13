b9728f4f9e1f0b07bb507d2482cf3b2b95ff14e4
veoliva <veolivatorre@gmail.com>
2023-09-12 21:12:56 -0300

    preentrega14 septiembre

--- preentrega14SepEjercicio1.js
+++ preentrega14SepEjercicio1.js
@@ -1,23 +1,24 @@
 //promp y alert (en pagina index) 
-
+/*
 
 let nombre = prompt ("ingrese su nombre ");
 let apellido = prompt ("ingrese su apellido ");
 alert ("hola" + " " + nombre + " " + apellido + " " + "bienvenido/a a la página del grupo D del curso ingenias");
-
+*/
 
 
 //  funcion donacion  ( en pagina index) (para ver el siguiente ejercicio comente el anterior /*)
 
-/*
-let donar = Number ( prompt ("deseas realizar una donación? \n1- SI \n2- NO"));
-  
-if (donar === 1 ) {
-alert ("para mas informacion  sobre como donar complete el formulario en la pagina de contacto");
-} 
-else {
-alert ("Muchas gracias por su visita");
-console.log("Muchas gracias por su visita");
-}
-*/
-     
+function donar() {
+   
+    let opcion = prompt("¿Desea donar? (1) Sí (2) No");
+
+    if (opcion === "1") {
+      alert("Para más información sobre cómo donar, complete el formulario en la página de contacto");
+    } else {
+      alert("Muchas gracias por su visita");
+    }
+  }
+   
+  donar(); 
+
