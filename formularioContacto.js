f9091077a222d3407f54b1ec740511fec5d82b29
veoliva <veolivatorre@gmail.com>
2023-10-02 19:12:26 -0300

    4ta preentrega segun reunion de zoom vero fer y guada!

--- formularioContacto.js
+++ formularioContacto.js
@@ -6,17 +6,16 @@ en la entrega anterior. Puede ser un texto, números, o
 combinación.
 -Mostrar al usuario dicha información (ya guardada en nuestra
 base de datos).
-//
-(en pagina de punto de reciclaje)
 -Crear estructura de datos en formato JSON, puede ser de
 productos, servicios o usuarios.
--Leerla desde un archivo JS y crear una función que nos deje
+-Leerla desde un archivo JS y 
+// (en pagina de campañas) crear una función que nos deje
 agregar un nuevo elemento a la base de datos, siguiendo la
 estructura de los elementos anteriores.*/
 
 
 
-window.onload = () => {
+window.onload = () =>{
 
 // Obtenemos los valores de los campos del formulario
 let nombre = document.querySelector("#nombre").value;
@@ -24,27 +23,32 @@ let apellido = document.querySelector("#apellido").value;
 let ciudad = document.querySelector("#ciudad").value;
 let email = document.querySelector("#email").value;
 let comentarios = document.querySelector("#comentarios").value;
-let submit= document.querySelector("#submit").value;
+
+// Creamos un objeto JSON con los datos del formulario
+let formData = {
+  nombre: nombre,
+  apellido: apellido,
+  ciudad: ciudad,
+  email: email,
+  comentarios: comentarios
+};
+
+// Convertimos el objeto a JSON
+let formDataJSON = JSON.stringify(formData);
 
 // Almacenamos la información en Storage
-localStorage.setItem("nombre", nombre);
-localStorage.setItem("apellido", apellido);
-localStorage.setItem("ciudad", ciudad);
-localStorage.setItem("email", email);
-localStorage.setItem("comentarios", comentarios);
+localStorage.setItem("formData", formDataJSON);
 
 // Mostramos la información almacenada en Storage al usuario
-document.querySelector("#nombre").textContent = localStorage.getItem("nombre");
-document.querySelector("#apellido").textContent = localStorage.getItem("apellido");
-document.querySelector("#ciudad").textContent = localStorage.getItem("ciudad");
-document.querySelector("#email").textContent = localStorage.getItem("email");
-document.querySelector("#comentarios").textContent = localStorage.getItem("comentarios");
+console.log("Formulario almacenado en localStorage:", localStorage.getItem("formData"));
 
 // Evento del botón submit
 document.querySelector("#submit").addEventListener("click", function() {
   // Mostramos la leyenda
   document.querySelector("#leyenda").textContent = "Su formulario ha sido entregado correctamente";
 
+  // Mostramos el objeto JSON por consola
+  console.log("Datos del formulario en formato JSON:", formDataJSON);
 });
 
 }
